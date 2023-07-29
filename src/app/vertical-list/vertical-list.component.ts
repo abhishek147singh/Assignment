7129d4ea85d84f32447b49f2af151eee120cfeaf
Ahishek kushwah <singhabhishek72764@gmail.com>
2023-07-29 11:28:37 +0530

    LeftArrowButton, RightArrowButton has been added to scroll FriendList.

--- src/app/vertical-list/vertical-list.component.ts
+++ src/app/vertical-list/vertical-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild , ElementRef } from '@angular/core';
 import { Input } from "@angular/core";
 
 @Component({
@@ -8,5 +8,19 @@ import { Input } from "@angular/core";
 })
 export class VerticalListComponent {
   @Input() listData : {ProfileImage:string , Name:string } [] = [];
+
+  @ViewChild('container') container : any;
+  
+  constructor(){}
+
+  public scrollRight(): void {
+    if(this.container !== undefined)
+      this.container.nativeElement.scrollTo({ left: (this.container.nativeElement.scrollLeft + 150), behavior: 'smooth' });
+  }
+
+  public scrollLeft(): void {
+    if(this.container !== undefined)
+      this.container.nativeElement.scrollTo({ left: (this.container.nativeElement.scrollLeft - 150), behavior: 'smooth' });
+  }
 
 }
